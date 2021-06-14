3ecdf68c975dea611c26c3431b08b981fa0150b6
Andy Nguyen <theofficialflow1996@gmail.com>
2021-06-14 19:19:00 +0200

    Clean up scripts patcher code.

--- loader/scripts_patch.h
+++ loader/scripts_patch.h
@@ -1,8 +1,6 @@
 #ifndef __SCRIPTS_PATCH_H__
 #define __SCRIPTS_PATCH_H__
 
-int check_original(SceUID fd, uint8_t *original_bytes, size_t size, SceOff offset);
-
 void patch_scripts(void);
 
 #endif
