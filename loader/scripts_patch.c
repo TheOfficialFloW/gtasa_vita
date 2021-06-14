77e8ccdbd9f63fd8be83a9639ef98ba29c830948
Unknown <syrota.nick@gmail.com>
2021-06-14 19:24:47 +0300

    Check original bytes instead

--- loader/scripts_patch.c
+++ loader/scripts_patch.c
@@ -4,40 +4,23 @@
 
 #include "dialog.h"
 #include "config.h"
-#include "sha1.h"
 
-static uint32_t scm_orig_sha1[5] = { 0xFAB33E12, 0xB50F7E6B, 0x3BA52119, 0x948FEB82, 0x31A55BB6 };
-static uint32_t img_orig_sha1[5] = { 0x8E314EED, 0x3E3EE789, 0xAC6A2A22, 0xCB1D8812, 0x80DADDDF };
+static uint8_t orig_opcode_00E1[6] = { 0xE1, 0x00, 0x04, 0x00, 0x04, 0x10 };
+static uint8_t orig_opcode_80E1[6] = { 0xE1, 0x80, 0x04, 0x00, 0x04, 0x10 };
 
-static uint8_t hid_pressed_nop[6] = { 0x90, 0x0A, 0x04, 0x40, 0x00, 0x00 };
-static uint8_t not_hid_pressed_nop[6] = { 0x90, 0x8A, 0x04, 0x40, 0x00, 0x00 };
+static uint8_t patch_0A90_nop[6] = { 0x90, 0x0A, 0x04, 0x40, 0x00, 0x00 };
+static uint8_t patch_8A90_nop[6] = { 0x90, 0x8A, 0x04, 0x40, 0x00, 0x00 };
 
-int hash_equal(SceUID fd, uint32_t *original_hash) {
-  uint32_t sha1[5];
-  SHA1_CTX ctx;
-
-  void *data;
-  size_t size;
-
+int check_original(SceUID fd, uint8_t *original_bytes, size_t size, SceOff offset) {
+  uint8_t read_bytes[size];
+  sceIoPread(fd, read_bytes, size, offset);
   int result = 1;
-
-  size = sceIoLseek(fd, 0, SCE_SEEK_END);
-  sceIoLseek(fd, 0, SCE_SEEK_SET);
-  data = malloc(size);
-  sceIoRead(fd, data, size);
-
-  sha1_init(&ctx);
-  sha1_update(&ctx, data, size);
-  sha1_final(&ctx, (uint8_t *)sha1);
-
-  for (int i = 0; i < 5; i++) {
-    if (sha1[i] != original_hash[i]) {
+  for (int i = 0; i < size; i++) {
+    if (read_bytes[i] != original_bytes[i]) {
       result = 0;
       break;
     }
   }
-  free(data);
-  
   return result;
 }
 
@@ -46,19 +29,21 @@ void patch_scripts(void) {
   if (scm < 0)
     fatal_error("Error could not load game scripts: %s.", SCRIPT_SCM_PATH); // it will crash anyway
 
-  if (hash_equal(scm, scm_orig_sha1)) {
-    sceIoPwrite(scm, &hid_pressed_nop, sizeof(hid_pressed_nop), 0x00066703); // ITB
-    sceIoPwrite(scm, &hid_pressed_nop, sizeof(hid_pressed_nop), 0x0006675A);
-  }
+  // ITB
+  if (check_original(scm, orig_opcode_00E1, sizeof(orig_opcode_00E1), 0x00066703))
+    sceIoPwrite(scm, &patch_0A90_nop, sizeof(patch_0A90_nop), 0x00066703);
+  if (check_original(scm, orig_opcode_00E1, sizeof(orig_opcode_00E1), 0x0006675A))
+    sceIoPwrite(scm, &patch_0A90_nop, sizeof(patch_0A90_nop), 0x0006675A);
   sceIoClose(scm);
 
   SceUID img = sceIoOpen(SCRIPT_IMG_PATH, SCE_O_RDWR, 0);
   if (img < 0)
     fatal_error("Error could not load game scripts: %s.", SCRIPT_IMG_PATH);
 
-  if (hash_equal(img, img_orig_sha1)) {
-    sceIoPwrite(img, &hid_pressed_nop, sizeof(hid_pressed_nop), 0x0004C0FF); // Gym treadmill
-    sceIoPwrite(img, &not_hid_pressed_nop, sizeof(not_hid_pressed_nop), 0x0004C131);
-  }
+  // Gym treadmill
+  if (check_original(img, orig_opcode_00E1, sizeof(orig_opcode_00E1), 0x0004C0FF))
+    sceIoPwrite(img, &patch_0A90_nop, sizeof(patch_0A90_nop), 0x0004C0FF);
+  if (check_original(img, orig_opcode_80E1, sizeof(orig_opcode_80E1), 0x0004C131))
+    sceIoPwrite(img, &patch_8A90_nop, sizeof(patch_8A90_nop), 0x0004C131);
   sceIoClose(img);
 }
--- loader/scripts_patch.h
+++ loader/scripts_patch.h
@@ -1,7 +1,7 @@
 #ifndef __SCRIPTS_PATCH_H__
 #define __SCRIPTS_PATCH_H__
 
-int hash_equal(SceUID fd, uint32_t *original_hash);
+int check_original(SceUID fd, uint8_t *original_bytes, size_t size, SceOff offset);
 
 void patch_scripts(void);
 
